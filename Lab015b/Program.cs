e47d8ced3a4f3193a9de46564f7d735644dee925
AnyAP <anya.papkova@inbox.ru>
2022-12-29 22:22:24 +0300

    Добавьте файлы проекта.

---
+++ Lab015b/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Text;
+using System.Text.Json;
+
+
+namespace Lab015b
+{
+    public class MyLogger
+    {
+        public void Log(string component, string message)
+        {
+            Console.WriteLine("Component: {0} Message: {1} ", component, message);
+        }
+    }
+    public class Person : IRepository<Person>
+    {
+        public string Name { get; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public Person GetPerson(string name)
+        {
+            return new Person(name, Age);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface IRepository<T> : IDisposable
+    where T : class
+    {
+        T GetPerson(string name); // получение одного человека по имени
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person tom = new Person("Tom", 37);
+            string json = JsonSerializer.Serialize(tom);
+            Console.WriteLine(json);
+            Person? restoredPerson = JsonSerializer.Deserialize<Person>(json);
+            Console.WriteLine(restoredPerson?.Name); // Tom
+
+            string path = @"somefile.txt";
+            string path2 = @"somefile.json";
+
+            File.WriteAllText(path, json);
+            File.WriteAllText(path2, json);
+        }
+    }
+}
---
+++ Lab015c/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lab015c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            (new Thread(() =>
+            {
+                Singleton rand1 = new Singleton(r.Next(1, 100));
+                rand1 = rand1.GetInstance();
+                Console.WriteLine(rand1.number);
+            })).Start();
+            Singleton rand2 = new Singleton(r.Next(1, 100));
+            rand2 = rand2.GetInstance();
+            Console.WriteLine(rand2.number);
+        }
+    }
+
+    public class Singleton
+    {
+        public static Singleton? singletonInstance;
+
+        public int number { get; set; }
+        public Singleton(int i)
+        {
+            number = i;
+        }
+        private static object syncRoot = new();
+
+        public Singleton GetInstance()
+        {
+            if (singletonInstance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (singletonInstance == null)
+                        singletonInstance = new Singleton(number);
+                }
+            }
+            return singletonInstance;
+        }
+    }
+}
